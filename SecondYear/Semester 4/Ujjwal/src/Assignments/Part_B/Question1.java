cdd4968df1a65261933ea62743f0a2e8b1534ab2
Kodiererin <iamujjwal45@gmail.com>
2023-04-09 14:57:34 +0530

    Adding Files

---
+++ SecondYear/Semester 4/Ujjwal/src/Assignments/Part_B/Question1.java
@@ -0,0 +1,28 @@
+package Assignments.Part_B;
+
+public class Question1 {
+    public static void main(String[] args) {
+        compute();
+    }
+    public static void compute(){
+        java.util.TreeSet<Integer> tree = new java.util.TreeSet<>();
+        tree.add(12);
+        tree.add(13);
+        tree.add(12);
+        tree.add(123);
+        tree.add(12);
+
+        // If the element is present in the tree set or not?
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        System.out.println("Enter the Number that You want to check ");
+        Integer x = sc.nextInt();
+        for (Integer treeSetData : tree) {
+            System.out.println(treeSetData);
+            if(treeSetData-x==0){
+                System.out.println("Element Found");
+                break;
+            }
+        }
+// Will be completed Later
+    }
+}
