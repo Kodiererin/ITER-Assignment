eec08bc85ffbc426413aa0206887e7494d02fea3
Kodiererin <iamujjwal45@gmail.com>
2023-04-28 02:46:37 +0530

    Adding Files

--- SecondYear/Semester 4/Ujjwal/src/April_26/d_MaximinixBitonicList.java
+++ SecondYear/Semester 4/Ujjwal/src/April_26/d_MaximinixBitonicList.java
@@ -4,5 +4,18 @@ package April_26;
 //	2,5,6,7,8,9,10,4,3,2
 //	iska Output 10 hoga kyuki uske baad value decrease hote jaa rha h!.
 public class d_MaximinixBitonicList {
-	
+	// Time Complexity in n time
+    public static void nTime_Find(int[] arr){
+        for(int i=0 ; i<arr.length-1; i++){
+            if(arr[i]>arr[i+1]){
+                System.out.print("BitIonic Element Found ");
+                System.out.println(arr[i]);
+                break;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {2,5,6,7,8,9,10,4,3,2};
+        nTime_Find(arr);
+    }
 }
--- SecondYear/Semester 4/Ujjwal/src/April_26/e_frequency_logNTime.java
+++ SecondYear/Semester 4/Ujjwal/src/April_26/e_frequency_logNTime.java
@@ -1,5 +1,6 @@
 package April_26;
 //	Given a Sorted Array, Find the Frequeny/Occurence of the element in logN time
+// 1,1,1,1,1,2,2,2,2,3,3,3,3
 public class e_frequency_logNTime {
 	public static void main(String[] args) {
 		
