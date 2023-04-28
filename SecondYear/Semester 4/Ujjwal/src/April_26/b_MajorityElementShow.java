e401deb54b5d5594d7f3200db62f56c0a86f76e6
Kodiererin <iamujjwal45@gmail.com>
2023-04-28 06:24:07 +0530

    Adding Files

--- SecondYear/Semester 4/Ujjwal/src/April_26/b_MajorityElementShow.java
+++ SecondYear/Semester 4/Ujjwal/src/April_26/b_MajorityElementShow.java
@@ -3,28 +3,26 @@ package April_26;
 //	Same Question but Do it in Log(n) time
 //	Many Test Cases are getting Failed.
 //	Check the Test Cases.
+
+//	Majority Element ka Condition aakhir hai Kya?
+//	Usi Hisab se Krna h!
+//	Hm maan rhe h ki majority element ka condition ye hoga ki woh element ho aadha se zaada cover kr rha h 
+//	wohi Majority element hoga.
+
+//	I am not able to know the condition!!!!!!!!!!!!!!!!!!!!!!!!
+
 public class b_MajorityElementShow {
 	public static void main(String[] args) {
 		int[] arr = {1,1,4,4,4,4,4,4};
 		int[] arr2 = {1,2,3,4,5,5,5,5,5,5,5};
-		System.out.println("Array Length is "+arr2.length);
-		findMajorityBinarySEARCH(arr2, 0, arr2.length-1, arr2[arr2.length/2]);
-//		findMajorityBinarySEARCH(arr, 0, arr.length-1, arr[arr.length/2]);
 	}
-	public static void findMajorityBinarySEARCH(int[] arr , int i , int j , int value) {
-		System.out.println(i+" "+j+" "+value);
-		if((i==j) && ((i-1)<0 || (j+1)>arr.length-1) && arr[i]==value)
-			System.out.println("Element Found");
-		else if((i!=j)) {
-			if(value<=arr[(int)Math.floor((i+j)/2)]) {
-				findMajorityBinarySEARCH(arr, 0, (int) Math.floor((i+j)/2), value);
-			}
-			else if(value>=arr[(int)Math.floor((i+j)/2)]){
-				findMajorityBinarySEARCH(arr,(int) Math.floor((i+j)/2)+1, arr.length-1, value);
-			}
-		}
-		else {
-			System.out.println("Element Not Found");
-		}
+	
+//	 Majority Kb Hoga, jb Majority elements 0 se mid ya usse zaada Honge.
+//	Sorted Array Hai toh soch ke Krna hoga.
+	
+	public static int Majority(int[] arr , int i , int j) {
+		int mid = (i+j)/2;
+		if()
 	}
+	
 }
---
+++ SecondYear/Semester 4/Ujjwal/src/April_26/g_getFrequency_logN_time.java
@@ -0,0 +1,45 @@
+package April_26;
+
+public class g_getFrequency_logN_time {
+	public static void main(String[] args) {
+		int[] arr = {1,1,1,1,3,3,3,4,5,6,6,7,7,8,8,8,8,8};
+		System.out.println(getFrequency(arr, 6));
+	}
+	public static int getFrequency(int[] arr , int element) {
+		System.out.println(getFirstOccurence(arr, 0, arr.length, element));
+		System.out.println(getLastOccurence(arr, 0, arr.length, element));
+		
+		return Math.abs(getLastOccurence(arr, 0, arr.length, element) - getFirstOccurence(arr, 0, arr.length ,element) + 1);
+	}
+	public static int getFirstOccurence(int[] arr , int i , int j , int element) {
+		if(i<=j) {
+			int mid = (i+j)/2;
+			if((mid==0 || arr[mid-1]<element) && arr[mid]==element) {
+				return mid;
+			}
+			else if(element<=arr[mid]) {
+				return getFirstOccurence(arr, i, mid-1, element);
+			}
+			else if (element>arr[mid]){
+				return getFirstOccurence(arr, mid+1, j, element);
+			}
+		}
+		return -1;
+	}
+	public static int getLastOccurence(int[] arr , int i , int j , int element) {
+		if(i<=j) {
+			int mid = (i+j)/2;
+			if((mid==0 || arr[mid+1]>element) && arr[mid]==element) {
+				return mid; 
+			}
+			else if(element<arr[mid]) {
+				return getLastOccurence(arr, i, mid-1, element);
+			}
+			else if(element>=arr[mid]) {
+				return getLastOccurence(arr, mid+1, j, element);
+			}
+		}
+		return -1;
+	}
+	
+}
