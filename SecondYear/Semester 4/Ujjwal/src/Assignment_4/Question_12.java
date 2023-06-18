c15b651e23d88b07172cdb63284028f57a41444a
Kodiererin <iamujjwal45@gmail.com>
2023-06-19 04:25:14 +0530

    Adding QuestionSolution

---
+++ SecondYear/Semester 4/Ujjwal/src/Assignment_4/Question_12.java
@@ -0,0 +1,24 @@
+package Assignment_4;
+
+public class Question_12 {
+	public static void main(String[] args) {
+		int[] arr1 = {1,2,3,4,1,2,3};
+		int[] arr2 = {1,2,-1,23,2};
+		int value = 0;
+		compute(arr1, arr2, value);
+	}
+	public static void compute(int[] arr1 , int[] arr2 , int value) {
+		for(int i=0 ; i<arr1.length ; i++) {
+			int flag=0;
+			for(int j=0 ; j<arr2.length ; j++) {
+				if(arr1[i]+arr2[j]==value) {
+					System.out.println(arr1[i]+" "+arr2[j]);
+					flag=1;
+					break;
+				}
+			}
+			if(flag==1)
+				break;
+		}
+	}
+}
