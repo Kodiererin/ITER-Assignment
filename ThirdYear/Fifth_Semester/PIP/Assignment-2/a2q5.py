0e98233ef0b30f7a2d2648fd2de67ee37cfb8577
Kodiererin <iamujjwal45@gmail.com>
2023-10-05 22:09:31 +0530

    Adding Python Assignment-2

---
+++ ThirdYear/Fifth_Semester/PIP/Assignment-2/a2q5.py
@@ -0,0 +1,9 @@
+# 5. Develop Python functions to produce the following outputs
+
+print("Printing Patterns")
+
+
+print("\t\t*\n\t*\t*\t*\n*\t*\t*\t*\t*\n\t*\t*\t*\n\t\t*")
+
+
+print("$ $ $ $ $ \n$\t$\n$\t$\n$\t$\n$\t$\n$ $ $ $ $")
---
+++ ThirdYear/Fifth_Semester/PIP/Assignment-2/a2q7.py
@@ -0,0 +1,4 @@
+# 7. Write a function areaTriangle that takes the lengths of three sides: side1, side2, and side3 of the
+# triangle as the input parameters and returns the area of the triangle as the output. Also, assert that sum
+# of the length of any two sides is greater than the third side. Write a function main that accepts inputs
+# from the user interactively and computes the area of the triangle using the function areaTriangle.
