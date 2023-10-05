6f4f2f70d7b30a07ee8b0ef8f948fa87d38d74ac
Kodiererin <iamujjwal45@gmail.com>
2023-10-05 22:27:00 +0530

    Adding Assignment Files

---
+++ ThirdYear/Fifth_Semester/PIP/Assignment-2/a2q16.py
@@ -0,0 +1,21 @@
+# 16. What will be the output of following code?
+# x = -5
+# def display(x):
+# print(x)
+# x = 5
+# print(x)
+# display(x)
+# print(x)
+# a. -5, 5, -5
+# b. -5, 5, 5
+# c. -5, -5, -5
+# d. None of these.
+
+
+x = -5
+def display(x):
+    print(x)
+    x = 5
+    print(x)
+display(x)
+print(x)
--- ThirdYear/Fifth_Semester/PIP/Assignment-2/a2q7.py
+++ ThirdYear/Fifth_Semester/PIP/Assignment-2/a2q7.py
@@ -2,3 +2,21 @@
 # triangle as the input parameters and returns the area of the triangle as the output. Also, assert that sum
 # of the length of any two sides is greater than the third side. Write a function main that accepts inputs
 # from the user interactively and computes the area of the triangle using the function areaTriangle.
+import math
+
+
+def areaTriangle(x,y,z):
+   if x+y>z or y+z>x or x+z>y:
+       s = (x + y + z) / 2
+       return math.sqrt(s * (s - x) * (s - y) * (s - z))
+   else:
+       return -1
+
+
+def main():
+    if __name__ == '__main__':
+        x, y, z = int(input("Enter The First Side")), int(input("Enter The Second  Side")), int(
+            input("Enter The Third Side"))
+        print(areaTriangle(x,y,z))
+
+main()
