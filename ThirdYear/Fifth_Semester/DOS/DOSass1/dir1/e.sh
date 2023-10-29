80bea0d581c584954e66ca0c1098ddc5395c8902
Kodiererin <iamujjwal45@gmail.com>
2023-10-29 18:47:42 +0530

    Addig DOS Assignment

---
+++ ThirdYear/Fifth_Semester/DOS/DOSass1/dir1/e.sh
@@ -0,0 +1,16 @@
+echo *
+echo ***
+echo '***'
+echo \***
+echo \*\*\*
+echo */*
+echo Don't do this
+echo “Don't do this” (Quotes of one kind protect quotes of other kind)
+echo Hello # world
+echo “ Hello # world”
+echo ' Hello # world'
+echo date
+echo 'date'
+echo “date”
+echo `date`
+
---
+++ ThirdYear/Fifth_Semester/DOS/DOSass2/darg.sh
@@ -0,0 +1,4 @@
+total_args="$#"
+echo "Total number of arguments: $total_args"
+echo "First argument: $1"
+echo "Second argument: $2"
